da455b75cdd0aea970c237171228f887105300a8
전병훈 <ifries8611@gmail.com>
2021-06-22 07:27:58 +0900

    panResponder를 통한 card의 재스쳐효과 추가, Animated로 card에 효과 추가

--- screens/Favorites/FavsPresenter.js
+++ screens/Favorites/FavsPresenter.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components/native";
-import {Dimensions} from "react-native";
+import {Dimensions, PanResponder, Animated} from "react-native";
 import {apiImage} from "../../api";
 
-const {width:WIDTH, height:HEIGHT} = Dimensions.get("window");
+const {width: WIDTH, height: HEIGHT} = Dimensions.get("window");
 
 const Container = styled.View`
     padding-top: 50px;
@@ -12,13 +12,6 @@ const Container = styled.View`
     align-items: center;
 `;
 
-const Card = styled.View`
-    top: 80px; 
-    height: ${HEIGHT / 1.5}px;
-    width: 90%;
-    position: absolute;
-    
-`;
 
 const Poster = styled.Image`
     border-radius: 20px;
@@ -26,15 +19,84 @@ const Poster = styled.Image`
     height: 100%;
 `;
 
-export default ({results}) => {
+const styles = {
+    top: 80,
+    height: HEIGHT / 1.5,
+    width: "90%",
+    position: "absolute"
+}
 
+export default ({results}) => {
+    const [topIndex, setTopIndex] = useState(0);
+    const position = new Animated.ValueXY();
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderMove: (evt, {dx, dy}) => {
+            position.setValue({x: dx, y: dy});
+        },
+        onPanResponderRelease: () => {
+            Animated.spring(position, {
+                toValue: {
+                    x: 0,
+                    y: 0
+                },
+                useNativeDriver: true
+            }).start();
+        }
+    });
+    const roationValues = position.x.interpolate({
+        inputRange: [-100, 0, 100],
+        outputRange: ["-5deg", "0deg", "5deg"],
+        extrapolate: "clamp"
+    });
+    const secondCardOpacity = position.x.interpolate({
+        inputRange: [-255, 0, 255],
+        outputRange: [1, 0.2, 1],
+        extrapolate: "clamp"
+    });
+    const secondCardScale = position.x.interpolate({
+        inputRange: [-255, 0, 255],
+        outputRange: [1, 0.8, 1],
+        extrapolate: "clamp"
+    });
     return (
         <Container>
-            {results.reverse().map(result => (
-                <Card key={result.id}>
-                    <Poster source={{uri: apiImage(result.poster_path)}} />
-                </Card>
-            ))}
+            {results.map((result, index) => {
+                if (index === topIndex) {
+                    return (
+                        <Animated.View style={{
+                            ...styles,
+                            zIndex: 1,
+                            transform: [{rotate: roationValues}, ...position.getTranslateTransform()]
+                        }} key={result.id} {...panResponder.panHandlers}>
+                            <Poster source={{uri: apiImage(result.poster_path)}}/>
+                        </Animated.View>
+                    )
+                } else if (index === topIndex + 1) {
+                    return (
+                        <Animated.View style={{
+                            ...styles,
+                            zIndex: -index,
+                            opacity: secondCardOpacity,
+                            transform: [{
+                                scale: secondCardScale
+                            }]
+                        }} key={result.id} {...panResponder.panHandlers}>
+                            <Poster source={{uri: apiImage(result.poster_path)}}/>
+                        </Animated.View>
+                    )
+                } else {
+                    return (
+                        <Animated.View style={{
+                            ...styles,
+                            zIndex: -index,
+                            opacity: 0
+                        }} key={result.id} {...panResponder.panHandlers}>
+                            <Poster source={{uri: apiImage(result.poster_path)}}/>
+                        </Animated.View>
+                    )
+                }
+            })}
         </Container>
     );
 };
